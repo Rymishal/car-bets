ff89006f2960104a5c8632ddc3f4166d2372a13a
pq-mykhailo-r <mykhailo.r@protoq.dev>
2025-10-31 17:30:28 +0200

    Initial commit

---
+++ src/main/java/com/unitalk/carbets/CarBetsApplication.java
@@ -0,0 +1,13 @@
+package com.unitalk.carbets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarBetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unitalk/carbets/CarBetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unitalk.carbets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
